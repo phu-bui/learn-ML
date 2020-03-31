8b048b51c50d491cb422af6c20a735dbc80eb206
phu-bui <phu-bui@github.com>
2020-03-31 11:40:26 +0700

    the commit

--- MachineLearning/softmarginsmv/softmarginsmv.py
+++ MachineLearning/softmarginsmv/softmarginsmv.py
@@ -1,23 +1,31 @@
+from __future__ import division, print_function, unicode_literals
+# list of points
 import numpy as np
-#import matplotlib.pyplot as plt
-#from scipy.spatial.distance import cdist
-np.random.seed(21)
-#from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import cdist
+from matplotlib.backends.backend_pdf import PdfPages
+np.random.seed(22)
 
-means = [[2, 2], [4, 1]]
-cov = [[.3, .2], [.2, .3]]
-N = 10
-X0 = np.random.multivariate_normal(means[0], cov, N)
+means = [[2, 2], [4, 2]]
+cov = [[.7, 0], [0, .7]]
+N = 20
+X0 = np.random.multivariate_normal(means[0], cov, N) # each row is a data point
 X1 = np.random.multivariate_normal(means[1], cov, N)
-X1[-1, :] = [2.7, 2]
-X = np.concatenate((X0.T, X1.T), axis = 1)
-y = np.concatenate((np.ones((1, N)), -1*np.ones((1, N))), axis = 1)
 
-from sklearn.svm import SVC
-C = 100
-clf = SVC(kernel = 'linear', C = C)
-clf.fit(X, y)
+with PdfPages('data.pdf') as pdf:
+    plt.plot(X0[:, 0], X0[:, 1], 'bs', markersize = 8, alpha = 1)
+    plt.plot(X1[:, 0], X1[:, 1], 'ro', markersize = 8, alpha = 1)
+    plt.axis('equal')
+    plt.ylim(0, 4)
+    plt.xlim(0, 5)
 
-w_sklearn = clf.coef_.reshape(-1, 1)
-b_sklearn = clf.intercept_[0]
-print(w_sklearn.T, b_sklearn)
+    # hide tikcs
+    cur_axes = plt.gca()
+    cur_axes.axes.get_xaxis().set_ticks([])
+    cur_axes.axes.get_yaxis().set_ticks([])
+
+    plt.xlabel('$x_1$', fontsize = 20)
+    plt.ylabel('$x_2$', fontsize = 20)
+    pdf.savefig()
+    # plt.savefig('logistic_2d.png', bbox_inches='tight', dpi = 300)
+    plt.show()
